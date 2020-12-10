bbb8f14cc14bc796aa7ec26c5715469bfd6e5dee
Kovács György <gyorgy.kovacs@metrosystems.net>
2020-12-10 15:10:08 +0200

    Solved day 10

--- 10/first.hs
+++ 10/first.hs
@@ -1,8 +1,13 @@
 -- Advent of Code 2020 Day 10 Part 1 by KovaxG
 -- https://adventofcode.com/2020/day/10
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+import qualified Util
+import qualified Data.List as List
 
-process :: String -> String
-process s = s
+main :: IO ()
+main = Util.runProcessInts $ \adapters ->
+  let diffs = map diff $ Util.sliding 1 2 $ List.sort $ maximum adapters + 3 : 0 : adapters
+  in show $ Util.count (==1) diffs * Util.count(==3) diffs
+
+diff :: [Int] -> Int
+diff [a, b] = b - a
--- 10/second.hs
+++ 10/second.hs
@@ -1,8 +1,20 @@
 -- Advent of Code 2020 Day 10 Part 2 by KovaxG
 -- https://adventofcode.com/2020/day/10
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+import qualified Util
+import qualified Data.List as List
 
-process :: String -> String
-process s = s
+main :: IO ()
+main = Util.runProcessInts $ \adapters ->
+  show $ product $ map (formula . length) $ List.sort $ Util.split '3' $ (=<<) show $ map diff $ Util.sliding 1 2 $ List.sort $ maximum adapters + 3 : 0 : adapters
+
+diff :: [Int] -> Int
+diff [a, b] = b - a
+
+formula :: Int -> Int
+formula n = case n of
+  0 -> 1
+  1 -> 1
+  2 -> 2
+  3 -> 4
+  4 -> 7
