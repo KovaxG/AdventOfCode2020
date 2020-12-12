03f2bf8a8efdd5d6153bb6c7d15af3d5fed156e6
Kovács György <gyorgy.kovacs@metrosystems.net>
2020-12-12 19:49:59 +0200

    Solved day 12

--- 12/first.hs
+++ 12/first.hs
@@ -1,8 +1,42 @@
 -- Advent of Code 2020 Day 12 Part 1 by KovaxG
 -- https://adventofcode.com/2020/day/12
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+import qualified Util
+import qualified Data.Maybe as Maybe
 
-process :: String -> String
-process s = s
+data Dir = N | S | E | W | L | R | F deriving (Show, Read)
+data Action = Action Dir Int deriving (Show)
+
+type State = (Int, (Int, Int))
+
+parseAction :: String -> Maybe Action
+parseAction [] = Nothing
+parseAction (c:nrStr) = do
+  nr <- Util.safeRead nrStr
+  dir <- Util.safeRead [c]
+  return $ Action dir nr
+
+main :: IO ()
+main = Util.runProcess $ show . manhattanDist . foldl move (0, (0,0)) . Maybe.mapMaybe parseAction . lines
+
+move :: State -> Action -> State
+move s@(a, (x,y)) (Action dir d) = case dir of
+  W -> (a, (x - d, y))
+  E -> (a, (x + d, y))
+  N -> (a, (x, y + d))
+  S -> (a, (x, y - d))
+  L -> (a + d, (x, y))
+  R -> (a - d, (x, y))
+  F -> (a, forward)
+  where
+    forward :: (Int, Int)
+    forward = (x + round (cos af * df), y + round (sin af * df))
+
+    af = deg2rad $ fromIntegral a
+    df = fromIntegral d
+
+deg2rad :: Double -> Double
+deg2rad d = (d * 2 * pi) / 360.0
+
+manhattanDist :: State -> Int
+manhattanDist (_, (x, y)) = abs x + abs y
--- 12/second.hs
+++ 12/second.hs
@@ -1,8 +1,46 @@
 -- Advent of Code 2020 Day 12 Part 2 by KovaxG
 -- https://adventofcode.com/2020/day/12
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+import           Util (Coord)
+import qualified Util
+import qualified Data.Maybe as Maybe
 
-process :: String -> String
-process s = s
+data Dir = N | S | E | W | L | R | F deriving (Show, Read)
+data Action = Action Dir Int deriving (Show)
+
+type State = (Coord, Coord)
+
+parseAction :: String -> Maybe Action
+parseAction [] = Nothing
+parseAction (c:nrStr) = do
+  nr <- Util.safeRead nrStr
+  dir <- Util.safeRead [c]
+  return $ Action dir nr
+
+main :: IO ()
+main = Util.runProcess $
+  show
+  . manhattanDist
+  . foldl move ((10, 1), (0,0))
+  . Maybe.mapMaybe parseAction
+  . lines
+
+move :: State -> Action -> State
+move s@((wx, wy), (sx, sy)) (Action dir d) = case dir of
+  W -> ((wx - d, wy), (sx, sy))
+  E -> ((wx + d, wy), (sx, sy))
+  N -> ((wx, wy + d), (sx, sy))
+  S -> ((wx, wy - d), (sx, sy))
+  L -> (rotate (mod d 360) (wx, wy), (sx, sy))
+  R -> (rotate (mod (-d) 360) (wx, wy), (sx, sy))
+  F -> ((wx, wy), (sx + wx * d, sy + wy * d))
+
+rotate :: Int -> Coord -> Coord
+rotate a (x, y) = case a of
+  0   -> (x, y)
+  90  -> (-y, x)
+  180 -> (-x, -y)
+  270 -> (y, -x)
+
+manhattanDist :: State -> Int
+manhattanDist (_, (x, y)) = abs x + abs y
