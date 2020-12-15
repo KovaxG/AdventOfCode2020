1c5402e78e04673b3177c4b0e025843618eaee9d
Kovács György <gyorgy.kovacs@metrosystems.net>
2020-12-16 01:09:05 +0200

    Solved half of day 15

--- 15/first.hs
+++ 15/first.hs
@@ -1,8 +1,36 @@
 -- Advent of Code 2020 Day 15 Part 1 by KovaxG
 -- https://adventofcode.com/2020/day/15
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+{-# LANGUAGE NamedFieldPuns #-}
 
-process :: String -> String
-process s = s
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+type Nr = Int
+type Index = Int
+
+data State = State
+  { past :: Map Nr Index
+  , prevNr :: Nr
+  , index :: Index
+  } deriving (Show)
+
+tick :: State -> State
+tick s@State{past, prevNr, index} =
+  s { prevNr = newNr
+    , index = newIndex
+    , past = Map.insert prevNr index past
+    }
+  where
+    newIndex = index + 1
+    newNr = maybe 0 (\old -> index - old)
+           $ Map.lookup prevNr past
+
+initState :: [Nr] -> State
+initState (x:xs) = State { past = Map.fromList $ zip (reverse xs) [1 ..], index = 1 + length xs, prevNr = x }
+
+main :: IO ()
+main = putStrLn $ show $ calculate [5,1,9,18,13,8,0]
+
+calculate :: [Nr] -> Nr
+calculate xs = last . map prevNr . take (2020 - length xs + 1) . iterate tick . initState . reverse $ xs
--- 15/second.hs
+++ 15/second.hs
@@ -1,8 +1,42 @@
 -- Advent of Code 2020 Day 15 Part 2 by KovaxG
 -- https://adventofcode.com/2020/day/15
 
-main :: IO ()
-main = readFile "input.txt" >>= putStrLn . process
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE BangPatterns #-}
 
-process :: String -> String
-process s = s
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+type Nr = Int
+type Index = Int
+
+data State = State
+  { past :: Map Nr Index
+  , prevNr :: Nr
+  , index :: Index
+  } deriving (Show)
+
+tick :: State -> State
+tick s@State{past, prevNr, index} =
+  s { prevNr = newNr
+    , index = newIndex
+    , past = Map.insert prevNr index past
+    }
+  where
+    newIndex = index + 1
+    newNr = maybe 0 (\old -> index - old)
+           $ Map.lookup prevNr past
+
+initState :: [Nr] -> State
+initState (x:xs) = State { past = Map.fromList $ zip (reverse xs) [1 ..], index = 1 + length xs, prevNr = x }
+
+main :: IO ()
+main = putStrLn $ show $ calculate [5,1,9,18,13,8,0]
+
+calculate :: [Nr] -> Nr
+calculate xs = prevNr . go . initState . reverse $ xs
+  where
+    go :: State -> State
+    go !s@State{index}
+      | index == 30000000 = s
+      | otherwise = go (tick s)
